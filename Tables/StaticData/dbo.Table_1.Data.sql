dd0afa003a894a91820d41a9c655600c06ab6ed4
AVM <andre.moreira@petrosoft.pt>
2024-05-06 13:29:20 +0100

    db1

--- Tables/StaticData/dbo.Table_1.Data.sql
+++ Tables/StaticData/dbo.Table_1.Data.sql
@@ -2,7 +2,7 @@
 GO
 INSERT dbo.Table_1(pkCol, col1, col2) VALUES (1, 'a', 'b');
 INSERT dbo.Table_1(pkCol, col1, col2) VALUES (2, 'c', 'd');
-INSERT dbo.Table_1(pkCol, col1, col2) VALUES (3, 'e', 'db2');
+INSERT dbo.Table_1(pkCol, col1, col2) VALUES (3, 'db1', 'f');
 
 GO
 SET IDENTITY_INSERT [dbo].[Table_1] OFF
