5816525c0c059b0b8f1a0f609d4e95e4ce0ae395
AVM <andre.moreira@petrosoft.pt>
2024-05-06 13:19:41 +0100

    tab1

---
+++ Tables/StaticData/dbo.Table_1.Data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Table_1] ON
+GO
+INSERT dbo.Table_1(pkCol, col1, col2) VALUES (1, 'a', 'b');
+INSERT dbo.Table_1(pkCol, col1, col2) VALUES (2, 'c', 'd');
+INSERT dbo.Table_1(pkCol, col1, col2) VALUES (3, 'e', 'f');
+GO
+SET IDENTITY_INSERT [dbo].[Table_1] OFF
+GO
---
+++ Tables/dbo.Table_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table_1] (
+  [pkCol] [int] IDENTITY,
+  [col1] [varchar](50) NULL,
+  [col2] [varchar](50) NULL,
+  CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED ([pkCol])
+)
+ON [PRIMARY]
+GO
